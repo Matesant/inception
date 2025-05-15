e9a3312cd4cbd8c7cbf1022c0d5250b073f97949
Mateus <mateussantos1528@hotmail.com>
2025-05-14 23:45:15 -0300

    wordpress and nginx working, change in mariadb docker compose

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,21 +1,67 @@
 #!/bin/bash
 
-# Instala WordPress se não estiver instalado
-if [ ! -f wp-config.php ]; then
-  echo "🔧 Baixando WordPress..."
-  wget https://wordpress.org/latest.tar.gz
-  tar -xzf latest.tar.gz --strip-components=1
-  rm latest.tar.gz
+set -e
+cd /var/www/wordpress
 
-  echo "⚙️ Configurando wp-config.php..."
-  cp wp-config-sample.php wp-config.php
+check_db() {
+  echo "⏳ Verificando banco de dados em ${WORDPRESS_DB_HOST}..."
+  mysql -h"${WORDPRESS_DB_HOST}" -u"${WORDPRESS_DB_USER}" -p"${WORDPRESS_DB_PASSWORD}" -e "SHOW DATABASES;" > /dev/null 2>&1
+  return $?
+}
 
-  sed -i "s/database_name_here/${WORDPRESS_DB_NAME}/" wp-config.php
-  sed -i "s/username_here/${WORDPRESS_DB_USER}/" wp-config.php
-  sed -i "s/password_here/${WORDPRESS_DB_PASSWORD}/" wp-config.php
-  sed -i "s/localhost/${WORDPRESS_DB_HOST}/" wp-config.php
+until check_db; do
+  echo "⌛ Aguardando o banco de dados..."
+  sleep 3
+done
+echo "✅ Banco disponível."
+
+# Baixa o WordPress se não estiver presente
+if [ ! -f wp-load.php ]; then
+  echo "⬇️ Baixando WordPress..."
+  wp core download --path=/var/www/wordpress --allow-root
 fi
 
-echo "🚀 Iniciando PHP-FPM..."
+# Instalação e configuração
+if ! wp core is-installed --allow-root --path=/var/www/wordpress; then
+  echo "🔧 Criando wp-config.php"
+  wp config create \
+    --dbname=${WORDPRESS_DB_NAME} \
+    --dbuser=${WORDPRESS_DB_USER} \
+    --dbpass=${WORDPRESS_DB_PASSWORD} \
+    --dbhost=${WORDPRESS_DB_HOST} \
+    --locale=pt_BR \
+    --allow-root \
+    --path=/var/www/wordpress
+
+  echo "🚀 Instalando WordPress"
+  wp core install \
+    --url="https://${DOMAIN_NAME}" \
+    --title="${WP_TITLE}" \
+    --admin_user="${WP_ADMIN}" \
+    --admin_password="${WP_ADMIN_PASS}" \
+    --admin_email="${WP_ADMIN_EMAIL}" \
+    --skip-email \
+    --allow-root \
+    --path=/var/www/wordpress
+
+  wp plugin install wpdiscuz --activate --allow-root --path=/var/www/wordpress
+  wp option update default_comment_status open --allow-root --path=/var/www/wordpress
+
+  wp post create \
+    --post_title="Bem-vindo ao Inception!" \
+    --post_content="Esse é um post de teste com comentários." \
+    --post_status=publish \
+    --comment_status=open \
+    --allow-root \
+    --path=/var/www/wordpress
+
+  wp user create \
+    "${WP_VIWER_USER}" "${WP_VIWER_EMAIL}" \
+    --role=subscriber \
+    --user_pass="${WP_VIWER_PASSWORD}" \
+    --allow-root \
+    --path=/var/www/wordpress
+fi
+
 mkdir -p /run/php
 exec php-fpm7.4 -F
